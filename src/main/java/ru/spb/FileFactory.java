3d65ebb25c3a7c6ac409d9a5589cf18c4a738ace
Andrey1981spb <andrey1981spb@gmail.com>
2019-03-28 14:51:31 +0300

    new changes

---
+++ src/main/java/ru/spb/FileFactory.java
@@ -0,0 +1,21 @@
+package ru.spb;
+
+import java.util.Random;
+
+public class FileFactory {
+
+    public FileFactory(){
+
+    }
+
+        String nameOfFile;
+
+        public String NameCreator (){
+            Random random = new Random();
+
+            nameOfFile = String.valueOf(random.nextInt(150));
+
+            return nameOfFile;
+        }
+
+}
